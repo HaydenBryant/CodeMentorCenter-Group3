c202b5659a5b69f8ad3d764f058062f41149e68c
Ryan Silva <RyandotSilva@gmail.com>
2019-06-20 06:26:22 -0400

    Add login validation

---
+++ validation/login.js
@@ -0,0 +1,29 @@
+const Validator = require("validator");
+const isEmpty = require("./isEmpty");
+
+const validateLoginInput = data => {
+	let errors = {};
+	console.log(data);
+
+	data.email = !isEmpty(data.email) ? data.email : "";
+	data.password = !isEmpty(data.password) ? data.password : "";
+
+	if (Validator.isEmpty(data.password)) {
+		errors.login = "Password is required";
+	}
+
+	if (!Validator.isEmail(data.email)) {
+		errors.login = "Email is not valid";
+	}
+
+	if (Validator.isEmpty(data.email)) {
+		errors.login = "Email is required";
+	}
+
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
+
+module.exports = validateLoginInput;
