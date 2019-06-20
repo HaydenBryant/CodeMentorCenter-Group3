ad668d9d5d85b36f66cca297d6b84584b8fc7a14
Ryan Silva <RyandotSilva@gmail.com>
2019-06-20 05:08:55 -0400

    Update server imports

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, (err) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log("Server is running on port: " + PORT)
-})
+app.listen(PORT, err => {
+	if (err) {
+		console.log(err);
+	}
+	console.log("Server is running on port: " + PORT);
+});
