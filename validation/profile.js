536737ffe26b20500cec2baeb2f495eae6ed281b
Ryan Silva <RyandotSilva@gmail.com>
2019-06-20 06:31:11 -0400

    Update TODO

--- validation/profile.js
+++ validation/profile.js
@@ -1,1 +1,16 @@
-// TODO
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+const validateProfileInput = data => {
+	let errors = {};
+
+	// TODO
+	// Add profile input validation
+
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
+
+module.exports = validateProfileInput;
