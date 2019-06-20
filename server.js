b028191b0d6921043dd20c547de8b63a44e7bded
Ryan Silva <RyandotSilva@gmail.com>
2019-06-20 05:21:51 -0400

    Connect DB to server

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(bodyParser.json());
 // DB Config
 const db = require("./config/keys").mongoURI;
 
+// Conect to MongoDB
+mongoose
+	.connect(db)
+	.then(() => console.log("MongoDB Connect"))
+	.catch(err => console.log(err));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, err => {
