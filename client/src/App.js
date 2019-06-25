276556cd59c34200fe5327fbcceb0771fb29e84b
Steve B <stevebarriere.student@careerdevs.com>
2019-06-25 12:23:18 -0400

    Refactored import components in app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
+import { Navbar, Landing, Mentors, Mentees } from './components/layout';
 import Register from './components/auth/Register';
-import Mentors from './components/layout/Mentors';
-import Mentees from './components/layout/Mentees';
+import Login from './components/auth/Login';
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/mentors' component={Mentors} />
-          <Route exact path='/mentees' component={Mentees} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
-);
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/mentors' component={Mentors} />
+              <Route exact path='/mentees' component={Mentees} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    );
+  }
+}
 
 export default App;
---
+++ client/src/components/layout/index.js
@@ -0,0 +1,6 @@
+import Navbar from './Navbar';
+import Landing from './Landing';
+import Mentors from './Mentors';
+import Mentees from './Mentees';
+
+export { Navbar, Landing, Mentors, Mentees };
