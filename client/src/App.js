844b5cf648ca0180f3bbd4d657c45e03a61d967b
Ryan Silva <RyandotSilva@gmail.com>
2019-06-28 18:31:39 -0400

    Add logic to check for expired token/set header

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,27 @@ import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { clearCurrentProfile } from "./actions/profileActions";
 
+// Check for token
+if (localStorage.jwtToken) {
+	// Set auth token header auth
+	setAuthToken(localStorage.jwtToken);
+	// Decode token and get user info and exp
+	const decoded = jwt_decode(localStorage.jwtToken);
+	// Set user and isAuthenticated
+	store.dispatch(setCurrentUser(decoded));
+
+	// Check for expired token
+	const currentTime = Date.now() / 1000;
+	if (decoded.exp < currentTime) {
+		// Logout User
+		store.dispatch(logoutUser());
+		// Clear current profile
+		store.dispatch(clearCurrentProfile());
+		// Redirect to login
+		window.location.href = "/login";
+	}
+}
+
 class App extends Component {
 	render() {
 		return (
