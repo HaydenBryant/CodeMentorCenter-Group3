124220f30482de2bd40b473393b88ffb86b8962f
Ryan Silva <RyandotSilva@gmail.com>
2019-06-29 15:49:32 -0400

    Add route to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,4 +39,23 @@ router.get(
 	}
 );
 
+// @route GET api/profile/all
+// @desc Get all profiles
+// @access Public
+
+router.get("/all", (req, res) => {
+	const errors = {};
+
+	Profile.find()
+		.populate("user", ["name"])
+		.then(profiles => {
+			if (!profiles) {
+				errors.noprofile = "There are no profiles";
+				return res.status(404).json(errors);
+			}
+			res.json(profiles);
+		})
+		.catch(err => res.status(404).json({ profile: "There are no profiles" }));
+});
+
 module.exports = router;
