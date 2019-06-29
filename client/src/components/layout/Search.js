463429b9b69bdcb35aa6f051d964b49db49b8bc8
Steve B <stevebarriere.student@careerdevs.com>
2019-06-29 09:44:38 -0400

    Added in API and JS scripts to search in users for coding languages.

--- client/src/components/layout/Search.js
+++ client/src/components/layout/Search.js
@@ -1,12 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 const Search = () => {
   return (
     <Fragment>
-      <form className='search' action=''>
-        <input type='search' placeholder='Find a mentor...' required />
-        <button type='submit'>Search</button>
+      <form className="search" action="">
+        <input
+          id="findPLanguage"
+          type="search"
+          placeholder="Search programming languages..."
+          required
+        />
+        <button type="submit" onclick="findPLanguage()">
+          Search
+        </button>
       </form>
+      <div id="displaySearchResults">~~~display search results here~~~</div>
     </Fragment>
   );
 };
