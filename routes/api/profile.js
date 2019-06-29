58dab047efb73168e1620d0b336d17a9f9830f19
Ryan Silva <RyandotSilva@gmail.com>
2019-06-29 15:49:04 -0400

    Add route to get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
 const passport = require("passport");
+
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
+// Load Profile Model
+const Profile = require("../../models/Profile");
+// Load User Model
+const User = require("../../models/User");
 
 // @route GET api/profile/test
 // @desc Tests profile route
 // @access Public
 
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+// @route GET api/profile
+// @desc Get current users profile
+// @access Private
+
+router.get(
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		const errors = {};
+
+		Profile.findOne({ user: req.user.id })
+			.populate("user", ["name", "avatar"])
+			.then(profile => {
+				if (!profile) {
+					errors.noprofile = "There is no profile for this user";
+					return res.status(404).json(errors);
+				}
+				res.json(profile);
+			})
+			.catch(err => res.status(404).json(err));
+	}
+);
 
 module.exports = router;
