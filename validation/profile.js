f9a9028ee62e289c2e7ad9037a6b30173811e0e3
Ryan Silva <RyandotSilva@gmail.com>
2019-06-29 15:43:16 -0400

    Update profile validation

--- validation/profile.js
+++ validation/profile.js
@@ -1,16 +1,76 @@
 const Validator = require("validator");
 const isEmpty = require("./is-empty");
 
-const validateProfileInput = data => {
+module.exports = function validateProfileInput(data) {
 	let errors = {};
 
-	// TODO
-	// Add profile input validation
+	data.handle = !isEmpty(data.handle) ? data.handle : "";
+	data.status = !isEmpty(data.status) ? data.status : "";
+	data.skills = !isEmpty(data.skills) ? data.skills : "";
+	data.cmcLink = !isEmpty(data.cmcLink) ? data.cmcLink : "";
+	data.speakingLanguages = !isEmpty(data.speakingLanguages)
+		? data.speakingLanguages
+		: "";
+	data.skillLevel = !isEmpty(data.skillLevel) ? data.skillLevel : "";
+	data.frameworks = !isEmpty(data.frameworks) ? data.frameworks : "";
+	data.codingLanguages = !isEmpty(data.codingLanguages)
+		? data.codingLanguages
+		: "";
+
+	if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+		errors.handle = "Handle needs to be between 2 and 4 characters";
+	}
+
+	if (Validator.isEmpty(data.handle)) {
+		errors.handle = "Profile handle is required";
+	}
+
+	if (Validator.isEmpty(data.status)) {
+		errors.status = "Status field is required";
+	}
+
+	if (Validator.isEmpty(data.skills)) {
+		errors.skills = "Skills field is required";
+	}
+
+	if (!isEmpty(data.website)) {
+		if (!Validator.isURL(data.website)) {
+			errors.website = "Not a valid URL";
+		}
+	}
+
+	if (!isEmpty(data.youtube)) {
+		if (!Validator.isURL(data.youtube)) {
+			errors.youtube = "Not a valid URL";
+		}
+	}
+
+	if (!isEmpty(data.twitter)) {
+		if (!Validator.isURL(data.twitter)) {
+			errors.twitter = "Not a valid URL";
+		}
+	}
+
+	if (!isEmpty(data.facebook)) {
+		if (!Validator.isURL(data.facebook)) {
+			errors.facebook = "Not a valid URL";
+		}
+	}
+
+	if (!isEmpty(data.linkedin)) {
+		if (!Validator.isURL(data.linkedin)) {
+			errors.linkedin = "Not a valid URL";
+		}
+	}
+
+	if (!isEmpty(data.instagram)) {
+		if (!Validator.isURL(data.instagram)) {
+			errors.instagram = "Not a valid URL";
+		}
+	}
 
 	return {
 		errors,
 		isValid: isEmpty(errors)
 	};
 };
-
-module.exports = validateProfileInput;
