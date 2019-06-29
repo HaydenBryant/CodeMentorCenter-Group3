28884ee1d824a9e445ab5d6dd77eda6ed16b1a0b
Ryan Silva <RyandotSilva@gmail.com>
2019-06-29 16:03:02 -0400

    Add route to create and edit profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -98,4 +98,80 @@ router.get("/user/:user_id", (req, res) => {
 		);
 });
 
+// @route POST api/profile
+// @desc Create or edit user profile
+// @access Private
+
+router.post(
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		const { errors, isValid } = validateProfileInput(req.body);
+
+		// Check Validation
+		if (!isValid) {
+			// Return any errors with 400 status
+			return res.status(400).json(errors);
+		}
+
+		// Get fields
+		const profileFields = {};
+		profileFields.user = req.user.id;
+		if (req.body.isAMentor) profileFields.isAMentor = req.body.isAMentor;
+		if (req.body.isAMentee) profileFields.isAMentee = req.body.isAMentee;
+		if (req.body.handle) profileFields.handle = req.body.handle;
+		if (req.body.website) profileFields.website = req.body.website;
+		if (req.body.skillLevel) profileFields.skillLevel = req.body.skillLevel;
+		if (req.body.location) profileFields.location = req.body.location;
+		if (req.body.bio) profileFields.bio = req.body.bio;
+		if (req.body.cmcLink) profileFields.cmcLink = req.body.cmcLink;
+		if (req.body.githubusername)
+			profileFields.githubusername = req.body.githubusername;
+		// Coding languages - Split into array
+		if (typeof req.body.codingLanguages !== "undefined") {
+			profileFields.codingLanguages = req.body.codingLanguages.split(",");
+		}
+		// Speaking languages - Split into array
+		if (typeof req.body.speakingLanguages !== "undefined") {
+			profileFields.speakingLanguages = req.body.speakingLanguages.split(",");
+		}
+		// Frameworks - Split into array
+		if (typeof req.body.frameworks !== "undefined") {
+			profileFields.frameworks = req.body.frameworks.split(",");
+		}
+
+		// Social
+		profileFields.social = {};
+		if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+		if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+		if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+		if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+		if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+		Profile.findOne({ user: req.user.id }).then(profile => {
+			if (profile) {
+				// Update
+				Profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+					{ new: true }
+				).then(profile => res.json(profile));
+			} else {
+				// Create
+
+				// Check if handle exists
+				Profile.findOne({ handle: profileFields.handle }).then(profile => {
+					if (profile) {
+						errors.handle = "That handle already exists";
+						res.status(400).json(errors);
+					}
+
+					// Save Profile
+					new Profile(profileFields).save().then(profile => res.json(profile));
+				});
+			}
+		});
+	}
+);
+
 module.exports = router;
